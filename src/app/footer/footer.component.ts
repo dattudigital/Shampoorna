bade4a1bf8fba5a51759cca163297cc9debddbd0
dattudigital <rajani@digitalrupay.com>
2018-11-09 15:28:54 +0530

    global search

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,19 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router'
-
+import {FooterServiceService} from '../services/footer-service.service'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
-  constructor(private router:Router) { }
+  selectedValue:'';
+  searchList:any=[];
+  titleStyle = "hidden";
+  cols: any[];
+  constructor(private router:Router,private service:FooterServiceService) { }
 
   ngOnInit() {
+      
+    this.cols = [
+      { field: 'vehicle_engineno', header: 'Engine No' },
+      { field: 'vehicle_name', header: 'Vehicle Name' },
+      { field: 'make_name', header: 'Make Name' },
+      { field: 'model_name', header: 'Model Name' },
+      { field: 'color_name', header: 'Color' },
+      { field: 'type_name', header: 'Type Name' },
+
+    ];
   }
   newSaleClick(){
     this.router.navigate(['dashboard']);
+  }
+
+  searchVehicleDetails(){
+    console.log(this.selectedValue);
+    console.log(this.selectedValue.length);
+    if(this.selectedValue.length>2){
+      this.titleStyle = "visible";
+      this.service.globalSearch(this.selectedValue).subscribe(res=>{
+        console.log(res.json().result);
+        if(res.json().status ==true){
+          this.searchList =res.json().result;
+          this.titleStyle = "visible";
+        }else{
+          this.searchList =[];
+          this.titleStyle = "hidden";
+        }
+       
+      })
+    } else{
+      this.searchList =[];
+          this.titleStyle = "hidden";
+    }
+    
+    
   }
 
 }
---
+++ src/app/services/footer-service.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment';
+import {Http} from '@angular/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FooterServiceService {
+
+  constructor(private http:Http) { }
+
+  public globalSearch(val:any){
+    console.log(val);
+    return this.http.get(environment.host + 'search/global/'+val)
+  }
+}
